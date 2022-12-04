a9cfc27c0e3fa8042805f03729456e52187d1d17
Bedir Yılmaz <bedir.yilmaz@tier.app>
2022-12-04 23:39:08 +0100

    get the counts of answers

--- day_2.py
+++ day_2.py
@@ -12,18 +12,15 @@ args = parser.parse_args()
 
 answer_scores = {"A": 1, "B": 2, "C": 3}
 winner_hands = {"A": "B", "B": "C", "C": "A"}
+aliases = {"A": "X", "B": "Y", "C": "Z"}
 
-aliases = {}
+most_frequent_answers = {}
 
 rounds = np.loadtxt(args.filename, dtype=str)
 
 for move in answer_scores.keys():
-    answers = rounds[:, 0] == move
+    answers = rounds[rounds[:, 0] == move][:, 1]
     values, counts = np.unique(answers, return_counts=True)
-    ind = np.argmax(counts)
-    most_freq_answer = values[ind]
-
-    aliases[winner_hands[move]] = most_freq_answer
 
 # score_for_answer = answer_scores[most_freq_answer]
 # total_score = (score_for_answer + 3) * counts[ind]
