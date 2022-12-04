8c8799c5ec425c642ad424bee7b486ea53f87897
Bedir Yılmaz <bedir.yilmaz@tier.app>
2022-12-04 22:18:12 +0100

    day 2 find what x y z mean

---
+++ day_2.py
@@ -0,0 +1,27 @@
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser(
+    prog="Day1 Part 2", description="Find how many total fruits the top three richest elves have", epilog="AOC 2022 Day 1"
+)
+
+parser.add_argument("-f", "--filename")
+args = parser.parse_args()
+
+answer_scores = {"A": 1, "B": 2, "C": 3}
+winner_hands = {"A": "B", "B": "C", "C": "A"}
+
+aliases = {}
+
+rounds = np.loadtxt(args.filename, dtype=str)
+
+for move in answer_scores.keys():
+    answers = (rounds[:, 0] == move)
+    values, counts = np.unique(answers, return_counts=True)
+    ind = np.argmax(counts)
+    most_freq_answer = values[ind]
+
+    aliases[winner_hands[move]]=most_freq_answer
+
+# score_for_answer = answer_scores[most_freq_answer]
+# total_score = (score_for_answer + 3) * counts[ind] 
